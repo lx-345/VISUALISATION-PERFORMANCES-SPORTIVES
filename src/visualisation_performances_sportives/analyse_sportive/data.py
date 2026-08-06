92ed360d6171ae4f52fe3857effac99e198cc53f
lx-345 <leo.panier55@gmail.com>
2026-08-06 10:01:15 +0200

    fix: suppression du https en dur dans data.py

--- src/visualisation_performances_sportives/analyse_sportive/data.py
+++ src/visualisation_performances_sportives/analyse_sportive/data.py
@@ -10,20 +10,24 @@ load_dotenv()
 
 def get_clean_data() -> pd.DataFrame:
     """
-    Récupère les données nettoyées directement depuis le bucket S3 Onyxia
+    Récupère les données nettoyées depuis le bucket S3
     et retourne un DataFrame pandas prêt pour l'analyse.
     """
-    chemin_s3 = "paleo/donnees_strava/activites_clean.csv"
+    # On ajoute le bucket "medas" au début du chemin
+    chemin_s3 = "medas/paleo/donnees_strava/activites_clean.csv"
 
-    # Configuration du système de fichiers S3 avec les identifiants du .env
+    # On récupère l'URL brute telle qu'elle est dans le fichier Kubernetes
+    endpoint = os.getenv("AWS_S3_ENDPOINT")
+
+    # Configuration du système de fichiers S3
     fs = s3fs.S3FileSystem(
-        client_kwargs={"endpoint_url": f"https://{os.getenv('AWS_S3_ENDPOINT')}"},
+        client_kwargs={"endpoint_url": endpoint},
         key=os.getenv("AWS_ACCESS_KEY_ID"),
         secret=os.getenv("AWS_SECRET_ACCESS_KEY"),
         token=os.getenv("AWS_SESSION_TOKEN"),
     )
 
-    # Lecture directe du CSV distant sans le télécharger localement
+    # Lecture directe du CSV distant
     with fs.open(chemin_s3, "rb") as f:
         df = pd.read_csv(f)
 
